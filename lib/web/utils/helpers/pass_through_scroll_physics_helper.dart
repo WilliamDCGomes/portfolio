2749bca0da13f8aa6923bcd663fbdb260720a343
William Douglas <williamdouglasgomes@hotmail.com>
2025-07-05 19:48:50 -0300

    teste

--- lib/web/utils/helpers/pass_through_scroll_physics_helper.dart
+++ lib/web/utils/helpers/pass_through_scroll_physics_helper.dart
@@ -2,15 +2,18 @@ import 'package:flutter/widgets.dart';
 
 class PassThroughScrollPhysicsHelper extends ClampingScrollPhysics {
   const PassThroughScrollPhysicsHelper({ super.parent });
+
   @override
   PassThroughScrollPhysicsHelper applyTo(ScrollPhysics? ancestor) =>
       PassThroughScrollPhysicsHelper(parent: buildParent(ancestor));
 
   @override
   double applyBoundaryConditions(ScrollMetrics position, double value) {
-    final hitTop = value < 0 && position.pixels <= position.minScrollExtent;
+    final hitTop    = value < 0 && position.pixels <= position.minScrollExtent;
     final hitBottom = value > 0 && position.pixels >= position.maxScrollExtent;
 
-    return (hitTop || hitBottom) ? 0.0 : super.applyBoundaryConditions(position, value);
+    if (hitTop || hitBottom) return value;
+
+    return 0.0;
   }
 }
